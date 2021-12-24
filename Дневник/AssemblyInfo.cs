d5ba855296d00b1354d3fbba314b512c786fb457
Astarte <72278018+astarte19@users.noreply.github.com>
2021-12-24 23:57:05 +0300

    Diary

--- Дневник/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using Xamarin.Forms.Xaml;
-using Xamarin.Forms;
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("materialdesignicons-webfont.ttf", Alias ="iconFont")]
--- Дневник/Common/IconFont.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace Дневник.Common
-{
-    static class IconFont
-    {
-        public const string Menu = "\U000f035c";
-    }
-}
