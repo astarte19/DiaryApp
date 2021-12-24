01b8b10a457e17446b1e9c27c567eac553f74de9
Astarte <72278018+astarte19@users.noreply.github.com>
2021-12-24 23:57:20 +0300

    Diary

---
+++ Дневник/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Дневник.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+     //   public string Category { get; set; }
+     //   public string Date { get; set; } = DateTime.Now.ToShortDateString();
+       // public int Priority { get; set; }
+    }
+}
---
+++ Дневник/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Дневник.ViewModels;
+
+namespace Дневник.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
